a03670f04abcebacc487363cc7ffeebe59fd68e8
brajraj99 <117092986+brajraj99@users.noreply.github.com>
2022-12-10 16:24:25 +0000

    Update Assignment_2.py

--- Assignment_2.py
+++ Assignment_2.py
@@ -33,3 +33,35 @@ def dataset(filename):
     df_b = df_a.transpose()
     
     return df_a, df_b
+
+
+# -----------------------------------------------------------------------------
+# Calling the function dataset() to read the csv data file and obtaining the
+# original data frame and transposed dataframe
+df, df_tran = dataset('API_19_DS2_en_csv_v2_4700503')
+
+# changing the header of the transposed datafram and populating it with the
+# country names
+header1 = df_tran.iloc[0]
+df_tran = df_tran[1:]
+df_tran.columns = header1
+
+# Extracting information from the transposed matrix for different countries
+# Creating a dataframe for India and changing the column names of that
+# dataframe as the Indicator name
+df_india = df_tran['India']
+# The header extracted below can be used for headers of different countries
+header_country = df_india.iloc[1]
+# Renaming the columns as Indicator names
+df_india.columns = header_country
+# Dropping the unwanted rows from the dataframe
+df_india = df_india.drop(['Indicator Name', 'Country Code', 'Indicator Code'])
+# Likewise we can create dataframes of United States
+df_usa = df_tran['United States']
+df_usa.columns = header_country
+df_usa = df_usa.drop(['Indicator Name', 'Country Code', 'Indicator Code'])
+# Likewise we can create dataframes of United States
+df_somalia = df_tran['Somalia']
+df_somalia.columns = header_country
+df_somalia = df_somalia.drop(
+    ['Indicator Name', 'Country Code', 'Indicator Code'])
